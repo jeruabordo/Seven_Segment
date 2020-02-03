40adbc6fba1ffc1328b352cc62d77ef462b2a069
jeruabordo <58618434+jeruabordo@users.noreply.github.com>
2020-02-03 14:23:43 +0800

    Add files via upload

---
+++ SevenSegment/SevenSegment/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lbl8 = New System.Windows.Forms.Label()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbl7 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Panel1.Controls.Add(Me.lbl8)
+        Me.Panel1.Controls.Add(Me.lbl5)
+        Me.Panel1.Controls.Add(Me.lbl3)
+        Me.Panel1.Controls.Add(Me.lbl6)
+        Me.Panel1.Controls.Add(Me.lbl2)
+        Me.Panel1.Controls.Add(Me.lbl4)
+        Me.Panel1.Controls.Add(Me.lbl7)
+        Me.Panel1.Controls.Add(Me.lbl1)
+        Me.Panel1.Location = New System.Drawing.Point(78, 28)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(124, 181)
+        Me.Panel1.TabIndex = 0
+        '
+        'lbl8
+        '
+        Me.lbl8.BackColor = System.Drawing.Color.LightGray
+        Me.lbl8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl8.Location = New System.Drawing.Point(105, 152)
+        Me.lbl8.Name = "lbl8"
+        Me.lbl8.Size = New System.Drawing.Size(11, 11)
+        Me.lbl8.TabIndex = 5
+        '
+        'lbl5
+        '
+        Me.lbl5.BackColor = System.Drawing.Color.LightGray
+        Me.lbl5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl5.Location = New System.Drawing.Point(23, 97)
+        Me.lbl5.Name = "lbl5"
+        Me.lbl5.Size = New System.Drawing.Size(11, 55)
+        Me.lbl5.TabIndex = 12
+        '
+        'lbl3
+        '
+        Me.lbl3.BackColor = System.Drawing.Color.LightGray
+        Me.lbl3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl3.Location = New System.Drawing.Point(89, 96)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(11, 55)
+        Me.lbl3.TabIndex = 11
+        '
+        'lbl6
+        '
+        Me.lbl6.BackColor = System.Drawing.Color.LightGray
+        Me.lbl6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl6.Location = New System.Drawing.Point(23, 29)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.Size = New System.Drawing.Size(11, 55)
+        Me.lbl6.TabIndex = 10
+        '
+        'lbl2
+        '
+        Me.lbl2.BackColor = System.Drawing.Color.LightGray
+        Me.lbl2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl2.Location = New System.Drawing.Point(88, 30)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(11, 55)
+        Me.lbl2.TabIndex = 4
+        '
+        'lbl4
+        '
+        Me.lbl4.BackColor = System.Drawing.Color.LightGray
+        Me.lbl4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl4.Location = New System.Drawing.Point(36, 152)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(51, 11)
+        Me.lbl4.TabIndex = 9
+        '
+        'lbl7
+        '
+        Me.lbl7.BackColor = System.Drawing.Color.LightGray
+        Me.lbl7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl7.Location = New System.Drawing.Point(36, 85)
+        Me.lbl7.Name = "lbl7"
+        Me.lbl7.Size = New System.Drawing.Size(51, 11)
+        Me.lbl7.TabIndex = 8
+        '
+        'lbl1
+        '
+        Me.lbl1.BackColor = System.Drawing.Color.LightGray
+        Me.lbl1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lbl1.Location = New System.Drawing.Point(36, 18)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(51, 11)
+        Me.lbl1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Green
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(93, 219)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.lblName.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(17, 282)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(221, 23)
+        Me.lblName.TabIndex = 2
+        Me.lblName.Text = "ABORDO, GELLYNE G."
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 900
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(279, 338)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "7 Segment"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lbl8 As Label
+    Friend WithEvents lbl5 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl6 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents lbl7 As Label
+    Friend WithEvents lbl1 As Label
+End Class
---
+++ SevenSegment/SevenSegment/Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+    Dim counter As Byte = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim btn1 As Button = Button1
+
+        If Button1.Text = "Start" Then
+
+            Timer1.Start()
+            Button1.Text = "Stop"
+            btn1.BackColor = Color.Red
+
+        ElseIf Button1.Text = "Stop" Then
+
+            Timer1.Stop()
+            counter += 0
+            Button1.Text = "Start"
+            btn1.BackColor = Color.Green
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        counter += 1
+
+        If counter = 1 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.Blue
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.LightGray
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 2 Then
+
+            lbl1.BackColor = Color.LightGray
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.LightGray
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.LightGray
+            lbl7.BackColor = Color.LightGray
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 3 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.LightGray
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.Blue
+            lbl6.BackColor = Color.LightGray
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 4 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.LightGray
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 5 Then
+
+            lbl1.BackColor = Color.LightGray
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.LightGray
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 6 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.LightGray
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 7 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.LightGray
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.Blue
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 8 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.LightGray
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.LightGray
+            lbl7.BackColor = Color.LightGray
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 9 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.Blue
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+        ElseIf counter = 10 Then
+
+            lbl1.BackColor = Color.Blue
+            lbl2.BackColor = Color.Blue
+            lbl3.BackColor = Color.Blue
+            lbl4.BackColor = Color.Blue
+            lbl5.BackColor = Color.LightGray
+            lbl6.BackColor = Color.Blue
+            lbl7.BackColor = Color.Blue
+            lbl8.BackColor = Color.LightGray
+
+            counter = 0
+
+        End If
+
+    End Sub
+
+End Class
